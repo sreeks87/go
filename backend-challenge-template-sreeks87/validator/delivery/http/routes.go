87156d835a40f7213118dbbfe22da2fb192bf976
sreeks87 <14892516+sreeks87@users.noreply.github.com>
2021-11-10 17:49:52 +0530

    Delete backend-challenge-template-sreeks87 directory

--- backend-challenge-template-sreeks87/validator/delivery/http/routes.go
+++
@@ -1,8 +0,0 @@
-package delivery
-
-import "github.com/gorilla/mux"
-
-func Route(r *mux.Router, controller *ValidatorController) {
-	r.HandleFunc("/new", controller.GetNewGame).Methods("GET")
-	r.HandleFunc("/validate", controller.Validate).Methods("POST")
-}
--- backend-challenge-template-sreeks87/validator/domain/generator.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-type Generator interface {
-	GenerateFruitPosition(int, int, int, int) (int, int, error)
-	GenerateID() int
-}
